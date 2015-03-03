1efc33776684375a81f15cad3d5926dbca6c35a9
yushilong <yushilong0921@126.com>
2015-03-03 17:55:51 +0800

    update

---
+++ ArapidLib/src/com/ArapidLib/common/Lists.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.ArapidLib.common;
+
+import java.util.List;
+
+public class Lists {
+	
+	/**
+	 * 判断集合是否为空
+	 * @param list
+	 * @return
+	 */
+	public static <T> boolean isBlank(List<T> list) {
+		
+		if (list==null || list.size()==0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * 判断集合是否不为空
+	 * @param list
+	 * @return
+	 */
+	public static <T> boolean isNoBlank(List<T> list) {
+		
+		if (list!=null && list.size()>0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
